f43d700aef5b20ca63ab0aaa74946d100a531f07
Johnny Benitez <johnnybeniitez@gmail.com>
2024-10-21 21:02:30 -0300

    *

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('agenda_consultorios', 'root', '5872', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     define: {
         freezeTableName: true,
         timestamps: false
