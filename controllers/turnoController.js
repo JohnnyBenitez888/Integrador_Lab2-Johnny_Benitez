a3b2787b69ec3f9d307d35daa428a599eb50660a
Johnny Benitez <johnnybeniitez@gmail.com>
2024-11-14 21:47:27 -0300

    *

--- controllers/turnoController.js
+++ controllers/turnoController.js
@@ -1,11 +1,15 @@
 const turno = require("../models/turno");
 
 exports.listarTurnos = async (req, res) => {
-    try {
-        const turnos = await turno.findAll();
-        res.render("allturnos.pug", { titulo: 'Secretaria', tituloMenu: "Turnos", turnos: turnos });
-    } catch (error) {
-        console.error("Error al obtener los turnos gg:", error);
-        res.status(500).send("Error al obtener los turnos");
-    }
-  };
+  try {
+    const turnos = await turno.findAll();
+    res.render("allturnos.pug", {
+      titulo: "Secretaria",
+      tituloMenu: "Turnos",
+      turnos: turnos,
+    });
+  } catch (error) {
+    console.error("Error al obtener los turnos gg:", error);
+    res.status(500).send("Error al obtener los turnos");
+  }
+};
